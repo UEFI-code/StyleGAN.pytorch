847d903e499793f934af6ff0c96471f8ca1d1aad
UEFI-code <zhuoshizhang@hotmail.com>
2025-04-12 15:32:55 +0900

    Add Gallery

---
+++ Gallery/bad_dis_1/bad_d.py
@@ -0,0 +1,35 @@
+import torch
+import torch.nn as nn
+
+class Discriminator(nn.Module):
+    def __init__(self, kernelSize = 3, stride = 3, dim = 512, padding = 3):
+        super(Discriminator, self).__init__()
+        self.activation = nn.ReLU()
+        self.down1 = nn.Conv2d(3, dim, kernelSize, stride, padding, bias=True)
+        self.down2 = nn.Conv2d(dim, dim, kernelSize, stride, padding, bias=True)
+        self.down3 = nn.Conv2d(dim, dim, kernelSize, stride, padding, bias=True)
+        self.down4 = nn.Conv2d(dim, dim, kernelSize, stride, padding, bias=True)
+        self.down5 = nn.Conv2d(dim, dim, kernelSize, stride, padding, bias=True)
+        self.down6 = nn.Conv2d(dim, dim, kernelSize, stride, padding, bias=True)
+        self.windup = nn.Conv2d(dim, 1, kernelSize, stride, padding, bias=True)
+
+    def forward(self, x):
+        x = self.down1(x)
+        x = self.activation(x)
+        x = self.down2(x)
+        x = self.activation(x)
+        x = self.down3(x)
+        x = self.activation(x)
+        x = self.down4(x)
+        x = self.activation(x)
+        # x = self.down5(x)
+        # x = self.activation(x)
+        # print(x.shape)
+        #x = self.down6(x)
+        x = self.windup(x) # the output is 4x4
+        return x
+
+if __name__ == '__main__':
+    discriminator = Discriminator()
+    image = torch.randn(1, 3, 192, 192)
+    print(discriminator(image).shape)
